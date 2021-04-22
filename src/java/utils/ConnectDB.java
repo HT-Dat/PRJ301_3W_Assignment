350580b36091250d93d3b87f72527ec8397731e8
Wolf1910 <dungnguyenquang221@gmail.com>
2021-04-22 16:45:19 +0700

    Add necessary Servlet

--- src/java/utils/ConnectDB.java
+++ src/java/utils/ConnectDB.java
@@ -5,10 +5,27 @@
  */
 package utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Gray
  */
 public class ConnectDB {
-        
+
+        public static Connection makeConnetion() {
+                try {
+                        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                        String url = "jdbc:sqlserver://localhost:1433;databaseName=NorthwindCopyDB";
+                        Connection con = DriverManager.getConnection(url, "Dung", "123456");
+                        if (con != null) {
+                                return con;
+                        }
+                } catch (ClassNotFoundException | SQLException e) {
+                        e.printStackTrace();
+                }
+                return null;
+        }
 }
