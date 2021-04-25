9dd05ed5c916e2bd53ff72f480498cd889e1d3ff
hnim1922 <71585912+hnim1922@users.noreply.github.com>
2021-04-25 11:20:11 +0700

    registion

--- src/java/controller/LoginServlet.java
+++ src/java/controller/LoginServlet.java
@@ -5,84 +5,127 @@
  */
 package controller;
 
+import account.AccountDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
- * @author Gray
+ * @author ASUS GAMING
  */
-@WebServlet(name = "AccountServlet", urlPatterns = {"/AccountServlet"})
+@WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
 public class LoginServlet extends HttpServlet {
 
-        /**
-         * Processes requests for both HTTP <code>GET</code> and
-         * <code>POST</code> methods.
-         *
-         * @param request servlet request
-         * @param response servlet response
-         * @throws ServletException if a servlet-specific error occurs
-         * @throws IOException if an I/O error occurs
-         */
-        protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-                throws ServletException, IOException {
-                response.setContentType("text/html;charset=UTF-8");
-                try (PrintWriter out = response.getWriter()) {
-                        /* TODO output your page here. You may use following sample code. */
-                        out.println("<!DOCTYPE html>");
-                        out.println("<html>");
-                        out.println("<head>");
-                        out.println("<title>Servlet AccountServlet</title>");                        
-                        out.println("</head>");
-                        out.println("<body>");
-                        out.println("<h1>Servlet AccountServlet at " + request.getContextPath() + "</h1>");
-                        out.println("</body>");
-                        out.println("</html>");
-                }
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException, ClassNotFoundException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String action = request.getParameter("action");
+        
+        //if a==null -> redirect to login_form.jsp
+        if(action == null){
+            response.sendRedirect("login_form.jsp");
         }
-
-        // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-        /**
-         * Handles the HTTP <code>GET</code> method.
-         *
-         * @param request servlet request
-         * @param response servlet response
-         * @throws ServletException if a servlet-specific error occurs
-         * @throws IOException if an I/O error occurs
-         */
-        @Override
-        protected void doGet(HttpServletRequest request, HttpServletResponse response)
-                throws ServletException, IOException {
-                processRequest(request, response);
+        
+        //if a = login
+        else if(action.equals("login")){
+            String username = request.getParameter("username");
+            String pass = request.getParameter("password");
+            AccountDAO dao = new AccountDAO();
+            
+            //check login
+            boolean isValid = dao.checkLogin(username, pass);
+            
+            //if isValid = false -> setAttribute("success", isValid) and dispatch to login_form.jsp
+            if(isValid == false){
+                request.setAttribute("success", isValid);
+                request.setAttribute("username", username);
+                request.getRequestDispatcher("login_form.jsp").forward(request, response);
+            }
+            
+            //else redirect to NovelServlet
+            else{
+                AccountDAO aDAO = new AccountDAO();
+                HttpSession session = request.getSession();
+                session.setAttribute("user", aDAO.getAccountByUsername(username));
+                response.sendRedirect("NovelServlet");
+            }
         }
-
-        /**
-         * Handles the HTTP <code>POST</code> method.
-         *
-         * @param request servlet request
-         * @param response servlet response
-         * @throws ServletException if a servlet-specific error occurs
-         * @throws IOException if an I/O error occurs
-         */
-        @Override
-        protected void doPost(HttpServletRequest request, HttpServletResponse response)
-                throws ServletException, IOException {
-                processRequest(request, response);
+        else if(action.equals("logout")){
+            HttpSession session = request.getSession(false);
+            session.invalidate();
+            response.sendRedirect("NovelServlet");
         }
+    }
 
-        /**
-         * Returns a short description of the servlet.
-         *
-         * @return a String containing servlet description
-         */
-        @Override
-        public String getServletInfo() {
-                return "Short description";
-        }// </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+     
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
 
 }
--- web/RegisterForm.jsp
+++ web/RegisterForm.jsp
@@ -1,17 +1,66 @@
 <%-- 
-    Document   : RegisterForm
-    Created on : Apr 22, 2021, 3:04:19 PM
-    Author     : Gray
+    Document   : register_form
+    Created on : 04-Mar-2021, 17:28:14
+    Aulabelor     : chiuy
 --%>
 
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
-        <head>
-                <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-                <title>JSP Page</title>
-        </head>
-        <body>
-                <h1>Hello World!</h1>
-        </body>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Register</title>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+        <link rel="stylesheet" href="styles/register.css"/>
+    </head>
+    <body>
+        <div class="modal">
+            <form action="RegisterServlet" method="POST" enctype="multipart/form-data" id="form" class="modal-content animate">
+                <input type="hidden" name="action" value="register"/>
+                <h1 class="modal-title">Register</h1>
+                <div>
+                    <label>Username</label>
+                    <input type="text" name="username" value="${username}" id="username" />
+                    <c:if test="${duplicatedUser != null}">
+                        <span id="dupUser" style="color:red">This username already exists</span>
+                    </c:if>
+                </div>
+                <div id="usernameMsg" style="visibility: hidden" class="error">
+                    Username can only be of length 4 to 20 characters
+                </div>
+                <label>Full name</label>
+                <input type="text" name="name" value="${name}" id="name" />
+                <div id="nameMsg" style="visibility:hidden" class="error">
+                    Full name can not be empty
+                </div>
+                <label>Email</label>
+                <input type="text" name="email" value="${email}" id="email" />
+                <c:if test="${duplicatedEmail != null}">
+                    <div id="duplicatedEmailMsg" class="error">This email address has already been used</div>
+                </c:if>
+                <div id="emailMsg" style="visibility:hidden" class="error">
+                    Invalid Email
+                </div>
+                <label>Password</label>
+                <input type="password" name="password" id="password" />
+                <div id="passMsg" style="visibility:hidden" class="error">
+                    Password can only be of length 5 to 32 characters
+                </div>
+                <label>Confirm password</label>
+                <input type="password" name="conpass" id="confpassword" />
+                <div id="confpassMsg" style="visibility:hidden" class="error">
+                    Confirm password did not match
+                </div>
+                <label><b>Avatar</b></label>
+                <input style="display: block" type="file" name="avatar" value="${avatar}" id="avatar" />
+                <div class="footer">
+                    <button type="submit">Register</button>
+                    <a class="goback" style="display: block;" href="NovelServlet"><b>&laquo; Click here to go
+                            back</b></a>
+                </div>
+            </form>
+        </div>
+        <script src="js/script.js" defer></script>
+    </body>
 </html>
