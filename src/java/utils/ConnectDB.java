b3e7346a134fd132bc1cf399fad2376416fbbcf1
Wolf1910 <dungnguyenquang221@gmail.com>
2021-04-25 10:27:32 +0700

    web.xml

--- src/java/utils/ConnectDB.java
+++ src/java/utils/ConnectDB.java
@@ -18,7 +18,7 @@ public class ConnectDB {
         public static Connection makeConnection() {
                 try {
                         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-                        String url = "jdbc:sqlserver://localhost:1433;databaseName=NorthwindCopyDB";
+                        String url = "jdbc:sqlserver://localhost:1433;databaseName=PRJ301_Assignment_G5";
                         Connection con = DriverManager.getConnection(url, "Dung", "123456");
                         if (con != null) {
                                 return con;
